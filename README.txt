b61d6c28877212382afec4e1562500cd93cbb975
mxrtaaa <marta.zaluska55@gmail.com>
2025-12-29 23:11:34 +0100

    Added README.txt, changed the planet to Earth

---
+++ README.txt
@@ -0,0 +1,11 @@
+Algorytmion 2013 zadanie 3
+https://algorytmion.ms.polsl.pl/storage/files/Zadania2013.pdf
+
+Napisz program rozwiązujący eksperymentalnie zagadnienie paradoksu urodzin.
+Zadanie to polega na znalezieniu takiej liczby osób, aby szanse na to,
+że wśród nich znajdą się dwie osoby urodzone w tym samym dniu roku były większe,
+niż szanse na to, że takich dwóch osób nie ma. Samego roku nie bierzemy pod uwagę.
+Zadanie to można zrealizować generując losową próbkę n osób (a dokładniej dat ich urodzin)
+i po jej wygenerowaniu sprawdzając wystąpienie pary. Aby doświadczenie dało miarodajne wyniki,
+dla każdego n należy przeprowadzić wiele prób (np. 20000).
+Postępując w ten sposób znajdź najmniejszą liczbę naturalną spełniającą warunki zadania.
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.Objects;
+
+final class Main {
+    void main(String[] args) {
+        Birthdays birthdays = new Birthdays(2);
+        System.out.println("Najmniejsza liczba osób, które spełniają warunek: " + birthdays.birthdayParadox());
+    }
+
+    class Birthdays {
+        int planet;
+        int daysInYear;
+
+        int[] generateRandomBirthdays(int n) {
+            int[] birthdays = new int[n];
+
+            for(int i = 0; i < n; ++i) {
+                birthdays[i] = (int)((double)this.daysInYear * Math.random()) + 1;
+            }
+
+            return birthdays;
+        }
+
+        double sameBirthdayChances(int n) {
+            int tests = 20000;
+            int areSame = 0;
+
+            for(int i = 0; i < tests; ++i) {
+                int[] birthdays = this.generateRandomBirthdays(n);
+
+                for(int j = 0; j < n - 1; ++j) {
+                    int k;
+                    for(k = j + 1; k < n; ++k) {
+                        if (birthdays[k] == birthdays[j]) {
+                            ++areSame;
+                            break;
+                        }
+                    }
+
+                    if (k < n) {
+                        break;
+                    }
+                }
+            }
+
+            return (double)areSame / (double)tests;
+        }
+
+        int birthdayParadox() {
+            int n = 2;
+
+            double chances;
+            do {
+                chances = this.sameBirthdayChances(n);
+                ++n;
+            } while(chances < (double)0.5F);
+
+            return n - 1;
+        }
+
+        Birthdays() {
+            this.planet = 2;
+            this.daysInYear = 365;
+        }
+
+        Birthdays(int planet) {
+            this.planet = planet;
+            switch (planet) {
+                case 0 -> this.daysInYear = 88;
+                case 1 -> this.daysInYear = 225;
+                case 2 -> this.daysInYear = 365;
+                case 3 -> this.daysInYear = 687;
+                case 4 -> this.daysInYear = 4333;
+                case 5 -> this.daysInYear = 10756;
+                case 6 -> this.daysInYear = 30708;
+                case 7 -> this.daysInYear = 60223;
+                default -> this.daysInYear = 365;
+            }
+
+        }
+    }
 }
